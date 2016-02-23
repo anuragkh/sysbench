e4412fb28d0f6a76b8dfe60d6961c839faca6ce4
Anurag Khandelwal <anuragk@berkeley.edu>
2016-02-23 13:26:08 -0800

    Added setup script for Elasticsearch

---
+++ elasticsearch/sbin/setup.sh
@@ -0,0 +1,34 @@
+
+# Assumes server is running Ubuntu; modify if running on a different distribution
+sudo apt-get update # sudo yum update
+
+sudo mkdir -p /mnt/data
+sudo chown elasticsearch:elasticsearch /mnt/data
+sudo mkdir /mnt/log
+sudo chown elasticsearch:elasticsearch /mnt/log
+
+# Change location of elasticsearch.yml accordingly
+echo "
+node:
+  name: elastic-${HOSTNAME}
+
+path:
+  data: /mnt/data
+  logs: /mnt/log
+	
+discovery:
+	type: ec2
+	ec2:
+		groups: elastic-benchmark
+		any_group: true
+		availability_zones: us-east-1b
+	
+cloud:
+	aws:
+		access_key: $AWS_ACCESS_KEY
+		secret_key: $AWS_SECRET_KEY
+		
+index:
+	number_of_shards: 40
+	numer_of_replicas: 0
+" | sudo tee /opt/bitnami/elasticsearch/config/elasticsearch.yml
